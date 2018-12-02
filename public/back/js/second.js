6711bae249ab764d213f45084a078d6156321ec8
qsbdx <1589352280@qq.com>
2018-12-02 11:19:55 +0800

    二级分类图片上传完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -3,7 +3,7 @@ $(function () {
     var currentPage = 1; // 当前页
     var pageSize = 5; // 每页条数
     render(); // 一进页面发送ajax请求，获取数据，渲染页面
-
+    // 1、渲染页面
     function render() {
         $.ajax({
             type: "get",
@@ -39,5 +39,68 @@ $(function () {
         })
 
     }
+
+    // 2、点击添加分类按钮，显示模态框
+    $('#addBtn').click(function () {
+        $('#addModal').modal('show');
+
+        // 发送请求，获取一级分类的全部数据
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategoryPaging",
+            // 通过写死 page 和 pageSize 模拟获取全部一级分类的接口
+            data: {
+                page: 1,
+                pageSize: 100,
+            },
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+
+                var htmlStr = template("dropdownTpl", info);
+                $('.dropdown-menu').html(htmlStr);
+            }
+        })
+
+
+    })
+
+    // 3. 给下拉列表的 a 添加点击事件(通过事件委托实现)
+    $('.dropdown-menu').on("click", 'a', function () {
+
+        // 获取文本值
+        var txt = $(this).text();
+        // 设置给按钮
+        $("#dropdownText").text(txt);
+
+
+        // 获取id
+        var id = $(this).data("id");
+        // 设置给隐藏域
+        $('[name="categoryId"]').val(id);
+
+        // 调用updateStatus更新 隐藏域 校验状态成 VALID
+
+
+    })
+
+    // 4. 配置文件上传插件, 让插件发送异步文件上传请求
+    $("#fileupload").fileupload({
+        dataType: "json",
+        // done: 表示文件上传完成回调函数
+        // e：事件对象
+        // data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+        done: function (e, data) {
+            // console.log(data);
+            // console.log(data.result); // 后台返回的对象
+
+            var picObj = data.result; // 后台返回的数据
+            // 获取图片地址，设置给 img src
+            var picUrl = picObj.picAddr;
+            $('#imgBox img').attr("src", picUrl);
+        }
+    });
+
+    // 5. 
 
 })
